ef1c32afb7f16d24f3e35aa6e4d17ba9be68f78e
FrasinyukGleb <glebfrasinyuk@yandex.ru>
2025-05-27 22:18:40 +0300

    Second GET method added
    Добавлен метод GET /submitData/?user__email=<email> , выводящий список данных обо всех объектах, которые пользователь с почтой <email> отправил на сервер.

--- src/api/v1/mountain_pass.py
+++ src/api/v1/mountain_pass.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, status
+from typing import List
+
+from fastapi import APIRouter, status, Query
 from starlette.responses import JSONResponse
 
 from src.db.db import db_dependency
@@ -27,3 +29,13 @@ async def get_mountain_pass_by_id(mountain_pass_id: int, session: db_dependency)
     if not (db_mountain_pass := await db.get_mountain_pass_by_id(mountain_pass_id, session)):
         raise ValueError(f'Не удалось найти запись с id {mountain_pass_id}')
     return await db.get_info_mountain_pass(db_mountain_pass, session)
+
+
+@mountain_pass_router.get('/')
+async def get_mountain_pass_by_user_email(
+    session: db_dependency,
+    user__email: str = Query(pattern=r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
+) -> List[PassShowSchema]:
+    if not (user := await db.get_user_by_email(user__email, session)):
+        raise ValueError(f'Пользователь {user__email} не найден!')
+    return await db.get_mountain_passes_by_user_id(user.id, session)
